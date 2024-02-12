37932f1ef06128216972a0ad7ce1cb1b19cc08a1
UAH_LAB_VM_1 <fluhlej@uah.edu>
2024-02-12 02:22:06 -0600

    updates

--- TEST1.sh
+++ TEST1.sh
@@ -2,24 +2,27 @@
 
 
 UNIXTIME=$(date +%s)
-
+LOC=
 
 
 #Use dumpcap to capture packets on the interface ins160 
 # and write the output to a file called ins160.pcap
 # NOTE the & at the end of the command to run it in the background
-dumpcap -i ins160 -w /test1/LOG_${UNIXTIME}.pcapng &
+dumpcap -i ens160 -w "${LOC}/LOG_${UNIXTIME}.pcapng" &
 
 #Start the ipsec tunnel between VM1 and VM2
 ipsec up VM1-VM2
 ipsec status
 
 # DO STUFF
-ping 10.0.51.2 -c 30
+ping 10.0.52.2 -c 30
 
 # Stop the ipsec tunnel between VM1 and VM2
 ipsec down VM1-VM2
 
 #Stop the dumpcap process
 killall dumpcap
+
+mv ../../../../"${LOC}/LOG_${UNIXTIME}.pcapng" LOGS/
+chmod 777 LOGS/"${LOC}/LOG_${UNIXTIME}.pcapng"
 
