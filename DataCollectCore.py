f0a0296b297dd517fa9b389e4d30a506fc3c0346
jfluhler <50679541+jfluhler@users.noreply.github.com>
2024-04-09 14:54:48 -0500

    comments in core python code

--- DataCollectCore.py
+++ DataCollectCore.py
@@ -62,9 +62,11 @@ if pLvl > 0:
 if bool(CoreConfig['MaxTimeS']):
     max_run_time = CoreConfig['MaxTimeS']
 else:
-    max_run_time = 3600
+    max_run_time = 3600  # 3600 seconds is 1 hour
 
+print("\n\n -----------------------------------------------")
 print("Max Run Time: " + str(max_run_time/60) + " minutes")
+print("----------------------------------------------- \n\n")
 
 # Define the Docker Client
 docker = DockerClient(compose_files=CoreConfig['compose_files'])
@@ -74,10 +76,16 @@ docker.compose.ps()
 FreshRun = False
 
 if FreshRun == False:
+    ## Stop Docker Containers (incase any are running)
+    docker.compose.down()
+
     ## Start Docker Containers
     if pLvl > 0:
         print(" -- Starting Docker Containers -- ")
+    # Start the Docker Containers    
     docker.compose.up(detach=True)
+
+    # Wait for the containers to start, probably not needed at all
     time.sleep(5)
 else:
     docker.compose.restart()
@@ -89,13 +97,15 @@ if True == True:
     docker.execute("carol", shlex.split("ip link set eth0 qlen 1000"))
     docker.execute("moon", shlex.split("ip link set eth0 qlen 1000"))
 
-    # Enable moon charon to accept IPSEC connections
+    # Enable moon to accept IPSEC connections
     if pLvl > 0:
         print(" -- Enable .charon deamon on moon-- ")
     docker.execute("moon", shlex.split("./charon"), detach=True)
     docker.execute("moon", shlex.split("swanctl --list-conns"))
 
     #Start Logging (Charon)
+    if pLvl > 0:
+        print(" -- Enable .charon deamon on Carol-- ")
     docker.execute("carol", shlex.split("./charon"), detach=True)
     docker.execute("carol", shlex.split("swanctl --list-conns"))
 
@@ -122,7 +132,9 @@ if True == True:
     M_steps = 0
     M_AddParams = ''
 
+    # Assign starting TC Constrains to variables
     for x, obj in CarolConfig.items():
+        # First Constraint should be the adjustable constraint
         if x == 'Constraint1':
             C_ctype = obj['Type']
             C_constraint = obj['Constraint']
@@ -131,35 +143,44 @@ if True == True:
             C_Max = obj['EndRange']
             C_units = obj['Units']
             C_steps = obj['Steps']
-            C_AddParams = obj['AddParams']
+            if bool(obj['AddParams']):
+                C_AddParams = obj['AddParams']
 
+            # Build the tc command string
             tc_string = ("tc qdisc " + "add" + " dev " + C_interface + " root " + C_ctype + " " +  
                 C_constraint + " " + str(C_Min) + C_units + " " + C_AddParams)
 
             if pLvl > 1:
                 print("Carol Starter Adjustable Constraint: " + tc_string)
+
+            # Execute the tc command    
             docker.execute("carol", shlex.split(tc_string))
 
+            # If MirrorMoon is True then execute the same command on Moon
             if CoreConfig['MirrorMoon'] == True:
                 docker.execute("moon", shlex.split(tc_string))
 
         else:
+            # If there are additional static constraints listed
             tmp_ctype = obj['Type']
             tmp_constraint = obj['Constraint']
             tmp_interface = obj['Interface']
             tmp_Min = obj['StartRange']
             tmp_units = obj['Units']
-            tmp_AddParams = obj['AddParams']
+            if bool(obj['AddParams']):
+                tmp_AddParams = obj['AddParams']
+            else:
+                tmp_AddParams = ''
 
             
-
+            # Check the type of constraint
             if tmp_ctype == C_ctype:
-                # If the constraint type is the same then we need to tack on the additional constraints
-                # forexample if we are using netem on both constraints EG delay and loss
-                # then we cannot add both, so we combine them into one command and update AddParams with the 
+                # If the constraint type is the same then we need to tack on the additional constraints to the primary constraint
+                # for example if we are using netem on both constraints (for example delay and loss)
+                # then we cannot add both. Instead we combine them into one command and update AddParams with the 
                 # additional constraints. Then we need replace the previous constraint
                 # tc qdisc replace dev eth0 root netem delay 200ms loss 10%
-                # but really the perefered placement would be the the AddParams field.
+                # Really the perefered placement for additional static constraints would be the the AddParams field.
 
                 C_AddParams = (C_AddParams + " " + tmp_constraint + " "  + str(tmp_Min) + tmp_units + " " + tmp_AddParams)
 
@@ -170,18 +191,25 @@ if True == True:
             else:
                 tc_string = ("tc qdisc add dev " + tmp_interface + " root " + tmp_ctype + " " + 
                     tmp_constraint + " " + str(tmp_Min) + tmp_units + " " + tmp_AddParams)
+            
+            
+
+            # Execute the tc command
+            docker.execute("carol", shlex.split(tc_string))
             if pLvl > 1:
                 print("Carol Starter Constraint: " + tc_string)
 
-            docker.execute("carol", shlex.split(tc_string))
-
+            # If MirrorMoon is True then execute the same command on Moon
             if CoreConfig['MirrorMoon'] == True:
                 docker.execute("moon", shlex.split(tc_string))
+                if pLvl > 1:
+                    print("Carol Starter Constraint Mirrored on Moon:" + tc_string)
             
 
     # Access Moon and add starting TC Constrains
     if bool(MoonConfig):
         for x, obj in MoonConfig.items():
+            # First Constraint should be the adjustable constraint (though currently the code only adjust constraints on carol)
             if x == 'Constraint1':
                 M_ctype = obj['Type']
                 M_constraint = obj['Constraint']
@@ -190,56 +218,70 @@ if True == True:
                 M_Max = obj['EndRange']
                 M_units = obj['Units']
                 M_steps = obj['Steps']
-                M_AddParams = obj['AddParams']
+                if bool(obj['AddParams']):
+                    M_AddParams = obj['AddParams']
 
+                # Build the tc command string
                 tc_string = ("tc qdisc add dev " + M_interface + " root " + M_ctype + " " +  
                     M_constraint + " " + str(M_Min) + M_units + " " + M_AddParams)
 
                 if pLvl > 1:
                     print("Moon Starter Adjustable Constraint: " + tc_string)
+
+                # Execute the tc command    
                 docker.execute("moon", shlex.split(tc_string))
+
             else:
+                # If there are additional static constraints listed
                 tmp_ctype = obj['Type']
                 tmp_constraint = obj['Constraint']
                 tmp_interface = obj['Interface']
                 tmp_Min = obj['StartRange']
                 tmp_units = obj['Units']
-                tmp_AddParams = obj['AddParams']
+                if bool(obj['AddParams']):
+                    tmp_AddParams = obj['AddParams']
+                else:
+                    tmp_AddParams = ''
 
+                # Check the type of constraint
                 if tmp_ctype == M_ctype:
-                    # If the constraint type is the same then we need to tack on the additional constraints
-                    # forexample if we are using netem on both constraints EG delay and loss
-                    # then we cannot add both, so we combine them into one command and update AddParams with the 
+                     # If the constraint type is the same then we need to tack on the additional constraints to the primary constraint
+                    # for example if we are using netem on both constraints (for example delay and loss)
+                    # then we cannot add both. Instead we combine them into one command and update AddParams with the 
                     # additional constraints. Then we need replace the previous constraint
                     # tc qdisc replace dev eth0 root netem delay 200ms loss 10%
-                    # but really the perefered placement would be the the AddParams field.
+                    # Really the perefered placement for additional static constraints would be the the AddParams field.
 
                     M_AddParams = (M_AddParams + " " + tmp_constraint + " "  + str(tmp_Min) + tmp_units + " " + tmp_AddParams)
 
                     if pLvl > 1:
                         print("Warning: New Constraint Type is the same as the adjustable constraint:" + tmp_ctype)
-
+                    
+                    # Build the tc command string with replace
                     tc_string = ("tc qdisc " + "replace" + " dev " + M_interface + " root " + M_ctype + " " +  
                         M_constraint + " " + str(M_Min) + M_units + " " + M_AddParams)
+                    
                 else:
+                    # Build the tc command string with add
                     tc_string = ("tc qdisc add dev " + tmp_interface + " root " + tmp_ctype + " " + 
                         tmp_constraint + " " + str(tmp_Min) + tmp_units + " " + tmp_AddParams)
 
                 if pLvl > 1:
                         print("Moon Starter Constraint: " + tc_string)
 
+                # Execute the tc command on moon
                 docker.execute("moon", shlex.split(tc_string))
 
+# Wait for the containers to have all settings applied.
 time.sleep(1)
 
 # Start Run Timer
 if pLvl > 0:
     print(" -- Starting Data Collection Run -- ")
+
+# Start the run timer
 startrun_tic = time.perf_counter()
 
-
-
-#START Constraint 1 Loop
 
 # Calculate a linear range of values for Carol Constraint 1
 C_vals = np.linspace(C_Min, C_Max, C_steps)
@@ -251,51 +293,65 @@ if pLvl > 1:
     print("Total Planned Iterations: " + str(len(C_vals)))
     print("Planned Values for Carol Constraint " + str(C_vals))
 
+#START Constraint 1 Loop
 for i in trange(len(C_vals)):
     
     
     #Start Wireshark Logging
 
+    # Start Internal Timer for Constraint 1 Loop
     L1_tic = time.perf_counter()
 
     # Update Carol Constraints (change)
     tc_string = ("tc qdisc change dev " + C_interface + " root " + C_ctype + " " + 
         C_constraint + " " + str(C_vals[i]) + C_units + " " + C_AddParams)
     
+    # Execute the tc command
     docker.execute("carol", shlex.split(tc_string))
-    if pLvl > 1:
+    if pLvl > 2:
         print("Updated Carol With: " + tc_string)
 
     if CoreConfig['MirrorMoon'] == True:
         # Update Moon Constraints (change) to match carol
         docker.execute("moon", shlex.split(tc_string))
-        if pLvl > 1:
+        if pLvl > 2:
             print("Updated Moon With: " + tc_string)
 
 
     # START Single Constraint Function
     # IPSEC LOOP N TIMES
     ipsec_N = CoreConfig['TC_Interations']
-    if pLvl > 1:
+    if pLvl > 2:
         print(" -- Begin IPSec Loop -- ")
     for j in trange(ipsec_N):
-        # Initiate IPSEC Connection
-        if pLvl > 3:
-            print("swanctl --initiate --child net")
+
         
         try:
+            # Initiate IPSEC Connection
+            if pLvl > 3:
+                print("swanctl --initiate --child net")
+
             docker.execute("carol", shlex.split("swanctl --initiate --child net"))
+        except:
+            if pLvl > 1:
+                print("Possible Error initiating IPSEC Tunnel ")
 
+        try:
             # Send data file
-            docker.execute("carol", shlex.split("ping -c 5 strongswan.moon.com"))
+            docker.execute("carol", shlex.split("ping -c 2 strongswan.moon.com"))
+        except:
+            if pLvl > 1:
+                print("Possible Error sending data in Tunnel ")
 
+        try:
             # Deactivate IPSEC Connection
             if pLvl > 3:
                 print("swanctl --terminate --ike home")
+
             docker.execute("carol", shlex.split("swanctl --terminate --ike home"))
         except:
             if pLvl > 1:
-                print("Possible Error in IPSEC Loop")
+                print("Possible Error terminating IPSEC Tunnel")
 
         # Check timer if > max time break loop
         if time.perf_counter() - startrun_tic > max_run_time:
@@ -307,14 +363,20 @@ for i in trange(len(C_vals)):
 
     #Stop Wireshark Logging
 
-    # Move Data Files from Carol to local machine (or volume) and rename
+
+
+    ## Move Data Files from Carol to local machine (or volume) and rename
+    # Create date_time string
     date_time = time.strftime("%Y%m%d_%H%M")
 
-    LogName = ("./charon." + date_time + "." + C_constraint + "_" + str(C_vals[i]) + 
-        "." + "iter_" + str(ipsec_N) + ".log")
+    # create log file name
+    LogName = ("./charon." + date_time + "-" + C_constraint + "_" + str(C_vals[i]) + 
+        C_units + "-"+ "iter_" + str(ipsec_N) + ".log")
+    
+    # Copy log file from Carol to local machine
     docker.copy(("carol", "/var/log/charon.log"), LogName)
 
-     #Reload Settings which forces a new Charon Log File
+    #Reload Settings which forces a new Charon Log File
     docker.execute("carol", shlex.split("echo 'newlog' > /var/log/charon.log"))
     docker.execute("carol", shlex.split("swanctl --reload-settings"))
 
@@ -326,21 +388,18 @@ for i in trange(len(C_vals)):
         print("Total Time: " + str(total_time) + " seconds")
         print("Last Run Time: " + str(L1_time) + " seconds")
         print("Estimated Remaining Time: " + str(EstRemTime) + " seconds")
-    
-    #check timer if > max time break loop
-    if time.perf_counter() - startrun_tic > max_run_time:
-        break
-
-   
-    #docker.execute("carol", shlex.split("rm /var/log/charon.log"))
-    #docker.execute("carol", shlex.split("touch /var/log/charon.log"))
 
     #save run stats to file
     file1 = open("runstats.txt","a")
     file1.writelines(LogName + "; " +
-        "Additional Params: " + C_AddParams + 
-        "; Total Time: " + str(total_time) + " seconds\n")
+        "Additional Params: " + C_AddParams + "; tc_command: " + tc_string +
+        "; IterationTime: " + str(L1_time) + " seconds" + 
+        "; Total Run Time: " + str(total_time) + " seconds\n")
     file1.close()
+
+    #check timer if > max time break loop
+    if time.perf_counter() - startrun_tic > max_run_time:
+        break
 
 #END Constraint 1 Loop
 
